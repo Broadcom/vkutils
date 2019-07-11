a0d37d4599906c78a0c1c68f877fd42424d7faee
Suji Velupillai <suji.velupillai@broadcom.com>
2019-07-11 10:37:05 -0700

    SOC-9872: elf2vk: Allow multiple input files to generate one bin file
    Modified as follows:
     - can take one or more input files
     - each input file must be associated with unique processor id
     - processor id is limited to 16 bits
     - processor id is inserted at top 16bits of 64bits start address
    
    Command usage:
       elf2vk -i <input elf file> -p <processorID> -o <output bin file>
    
    Change-Id: I08895e4d526bab28f360a2697579271452e5f398
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/96406
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Suji Velupillai <suji.velupillai@broadcom.com>
    Tested-by: Suji Velupillai <suji.velupillai@broadcom.com>
    Reviewed-by: Shamez Kurji <shamez.kurji@broadcom.com>

--- elf2vk/elf2vk.c
+++ elf2vk/elf2vk.c
@@ -10,12 +10,14 @@
 #include <libelf.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 /*
  * Convert elf file to VK binary of load records of form:
  * MAGIC  (U64) - VK Image indicator
  * LENGTH (U64) - Length of remainder of image (including MAGIC at end)
+ * --------------- Processor 0 Section Info --------
  * ADDR0 (U64)  - Section 0 load address
  * SIZE0 (U32)  - Section 0 size
  * DATA0[] (U8) - Section 0 bytes to load
@@ -24,13 +26,41 @@
  * ADDR0 (U64)  - Section N load address
  * SIZE0 (U32)  - Section N size
  * DATA0[] (U8) - Section N bytes to load
- * ENTRY_ADDR (U32) - Entry Address to jump to
+ * ENTRY_ADDR0 (U64) - Higher 16bits contains processor number ID
+ *                   - Lower 48bits contains the address to jump to
  * SIZE0 (U32)  - Entry size always zero
+ * ---------------- Processor X Section Info -------
+ * ADDRX (U64)  - Section 0 load address
+ * SIZEX (U32)  - Section 0 size
+ * DATAX[] (U8) - Section 0 bytes to load
+ * ..
+ * ..
+ * ADDRX (U64)  - Section N load address
+ * SIZEX (U32)  - Section N size
+ * DATAX[] (U8) - Section N bytes to load
+ * ENTRY_ADDRX (U64) - Higher 16bits contains processor number ID
+ *                   - Lower 48bits contains the address to jump to
+ * SIZEX (U32)  - Entry size always zero
+ * -------------------------------------------------
  * MAGIC (U64) - VK Image Indicator
  */
 
 /* MAGIC field placed at start/end of binary */
 #define MAGIC 0x1234567800000000
+#define MAX_NPROCESSOR		8
+#define MAX_FILENAME_LEN	256
+#define PROCESSOR_ID_SHIFT	48
+#define BIT_ULL(x)		(1ULL << (x))
+#define ENTRY_ADDR_MASK		(BIT_ULL(PROCESSOR_ID_SHIFT) - 1)
+
+static void usage(const char *prog)
+{
+	errx(EXIT_FAILURE, "%s: %s\n"
+	     "\t -i infile1 -p processorID1\n"
+	     "\t ...\n"
+	     "\t -i infile%d -p processorID%d\n"
+	     "\t -o outfile", __func__, prog, MAX_NPROCESSOR, MAX_NPROCESSOR);
+}
 
 int main(int argc, char **argv)
 {
@@ -44,37 +74,68 @@ int main(int argc, char **argv)
 	uint8_t *buf;
 	uint32_t sz;
 	uint64_t addr;
-	uint32_t entry_addr;
+	uint64_t entry_addr;
 	uint64_t magic = MAGIC;
 	uint64_t length;
+	char infiles[MAX_NPROCESSOR][MAX_FILENAME_LEN];
+	uint16_t processorid[MAX_NPROCESSOR];
+	char outfile[MAX_FILENAME_LEN];
+	int c;
 
 	int verbose = 1;
+	uint16_t nfiles = 0;
+	uint16_t nprocessors = 0;
+	int outfile_flag = 0;
+	int x, y;
 
-	if (argc != 3)
-		errx(EXIT_FAILURE, "usage: %s infile outfile", argv[0]);
+	while ((c = getopt(argc, argv, "i:p:o:")) != -1) {
+		switch (c) {
+		case 'i':
+			if (strlen(optarg) >= MAX_FILENAME_LEN)
+				errx(EXIT_FAILURE,
+				     "ERROR: Input filename length exceeded %zu > %d\n",
+				     strlen(optarg), MAX_FILENAME_LEN);
+			strcpy(&infiles[nfiles++][0], optarg);
+			break;
+		case 'p':
+			if (atoi(optarg) > UINT16_MAX)
+				errx(EXIT_FAILURE,
+				    "ERROR: Processor ID exceeds UINT16_MAX\n");
+			processorid[nprocessors++] = atoi(optarg);
+			break;
+		case 'o':
+			if (strlen(optarg) >= MAX_FILENAME_LEN)
+				errx(EXIT_FAILURE,
+				     "ERROR: Output filename length exceeded %zu > %d\n",
+				     strlen(optarg), MAX_FILENAME_LEN);
+			strcpy(&outfile[0], optarg);
+			outfile_flag = 1;
+			break;
+		case '?':
+			usage(argv[0]);
+		}
+	}
+
+	/* Input files must have equal no of processor IDs to associate with */
+	if (nfiles != nprocessors || nfiles == 0 || outfile_flag == 0)
+		usage(argv[0]);
+
+	/* Check if processor IDs are unique */
+	for (x = 0; x < nprocessors; x++) {
+		for (y = 0; y < x; y++) {
+			if (processorid[x] == processorid[y])
+				errx(EXIT_FAILURE,
+				     "ERROR: Processor ID must be unique!\n");
+		}
+	}
 
 	if (elf_version(EV_CURRENT) == EV_NONE)
 		errx(EXIT_FAILURE, "libelf init failed: %s", elf_errmsg(-1));
 
-	/* open elf input file */
-	fd_in = open(argv[1], O_RDONLY, 0);
-	if (fd_in < 0)
-		errx(EXIT_FAILURE, "open \%s\" failed", argv[1]);
-
-	elf_in = elf_begin(fd_in, ELF_C_READ, NULL);
-	if (elf_in == NULL)
-		errx(EXIT_FAILURE, "elf_begin() failed: %s.", elf_errmsg(-1));
-
-	if (elf_kind(elf_in) != ELF_K_ELF)
-		errx(EXIT_FAILURE, "%s must be an ELF file.\n", argv[1]);
-
-	if (gelf_getehdr(elf_in, &ehdr) == NULL)
-		errx(EXIT_FAILURE, "getehdr() failed: %s", elf_errmsg(-1));
-
 	/* open output file */
-	fd_out = fopen(argv[2], "w");
+	fd_out = fopen(outfile, "w");
 	if (fd_out == NULL)
-		errx(EXIT_FAILURE, "open \"%s\" failed", argv[2]);
+		errx(EXIT_FAILURE, "open \"%s\" failed", outfile);
 
 	if (verbose)
 		printf("MAGIC=0x%jx\n", MAGIC);
@@ -86,75 +147,107 @@ int main(int argc, char **argv)
 	length = 0;
 	fwrite(&length, sizeof(length), 1, fd_out);
 
-	scn = NULL;
+	for (c = 0; c < nfiles; c++) {
+		/* open elf input file */
+		fd_in = open(infiles[c], O_RDONLY, 0);
+		if (fd_in < 0)
+			errx(EXIT_FAILURE, "open \%s\" failed", infiles[c]);
 
-	while ((scn = elf_nextscn(elf_in, scn)) != NULL) {
-		if (gelf_getshdr(scn, &shdr) != &shdr)
-			errx(EXIT_FAILURE,
-			     "getshdr() failed %s.", elf_errmsg(-1));
+		elf_in = elf_begin(fd_in, ELF_C_READ, NULL);
+		if (elf_in == NULL)
+			errx(EXIT_FAILURE, "elf_begin() failed: %s.",
+			     elf_errmsg(-1));
 
-		if (!(shdr.sh_flags & SHF_ALLOC)) {
-			/* Section not allocated, so nothing to write */
-			continue;
-		}
+		if (elf_kind(elf_in) != ELF_K_ELF)
+			errx(EXIT_FAILURE, "%s must be an ELF file.\n",
+			     infiles[c]);
 
-		data = NULL;
-		data = elf_getdata(scn, data);
-		if (data == NULL)
-			data = elf_rawdata(scn, data);
-		if (data == NULL)
-			errx(EXIT_FAILURE,
-			     "getdata failed %s.", elf_errmsg(-1));
+		if (gelf_getehdr(elf_in, &ehdr) == NULL)
+			errx(EXIT_FAILURE, "getehdr() failed: %s",
+			     elf_errmsg(-1));
 
-		if ((data->d_size <= 0) | (data->d_buf == NULL)) {
-			/* No data to write in section */
-			continue;
-		}
+		scn = NULL;
 
-		addr = shdr.sh_addr;
-		sz = data->d_size;
-		buf = data->d_buf;
-
-		/* Debug info printing section generation information */
-		if (verbose) {
-			size_t shstrndx;
-			char *name;
-
-			printf("ADDR=0x%016" PRIx64 " SZ=0x%8.8x ", addr, sz);
-
-			if (elf_getshdrstrndx(elf_in, &shstrndx) != 0)
+		while ((scn = elf_nextscn(elf_in, scn)) != NULL) {
+			if (gelf_getshdr(scn, &shdr) != &shdr)
 				errx(EXIT_FAILURE,
-				     "getshdrstrndx failed %s.",
-				     elf_errmsg(-1));
+				     "getshdr() failed %s.", elf_errmsg(-1));
 
-			name = elf_strptr(elf_in, shstrndx, shdr.sh_name);
-			if (name == NULL)
+			if (!(shdr.sh_flags & SHF_ALLOC)) {
+				/* Section not allocated, so nothing to write */
+				continue;
+			}
+
+			data = NULL;
+			data = elf_getdata(scn, data);
+			if (data == NULL)
+				data = elf_rawdata(scn, data);
+			if (data == NULL)
 				errx(EXIT_FAILURE,
-				     "strptr() failed %s.", elf_errmsg(-1));
+				     "getdata failed %s.", elf_errmsg(-1));
 
-			printf("Section %-4.4jd %s\n", elf_ndxscn(scn), name);
+			if ((data->d_size <= 0) || (data->d_buf == NULL)) {
+				/* No data to write in section */
+				continue;
+			}
+
+			addr = shdr.sh_addr;
+			sz = data->d_size;
+			buf = data->d_buf;
+
+			/* Debug info printing section generation information */
+			if (verbose) {
+				size_t shstrndx;
+				char *name;
+
+				printf("ADDR=0x%016" PRIx64 " SZ=0x%8.8x ",
+				       addr, sz);
+
+				if (elf_getshdrstrndx(elf_in, &shstrndx) != 0)
+					errx(EXIT_FAILURE,
+					     "getshdrstrndx failed %s.",
+					     elf_errmsg(-1));
+
+				name = elf_strptr(elf_in, shstrndx,
+						  shdr.sh_name);
+				if (name == NULL)
+					errx(EXIT_FAILURE,
+					     "strptr() failed %s.",
+					     elf_errmsg(-1));
+
+				printf("Section %-4.4jd %s\n",
+				       elf_ndxscn(scn), name);
+			}
+
+			/* Write ADDR, SZ, and DATA to binary file */
+			fwrite(&addr, sizeof(addr), 1, fd_out);
+			length += sizeof(addr);
+			fwrite(&sz, sizeof(sz), 1, fd_out);
+			length += sizeof(sz);
+			fwrite(buf, sizeof(*buf), sz, fd_out);
+			length += sizeof(*buf) * sz;
 		}
 
-		/* Write ADDR, SZ, and DATA to binary file */
-		fwrite(&addr, sizeof(addr), 1, fd_out);
-		length += sizeof(addr);
+		/* Write processor ID and entry addr */
+		entry_addr = processorid[c];
+		entry_addr = (entry_addr << PROCESSOR_ID_SHIFT) | ehdr.e_entry;
+		if (verbose)
+			printf("ProcessorID = %u ENTR_ADDR=0x%llx\n",
+			       (uint16_t)(entry_addr >> PROCESSOR_ID_SHIFT),
+			       (entry_addr & ENTRY_ADDR_MASK));
+
+		fwrite(&entry_addr, sizeof(entry_addr), 1, fd_out);
+		length += sizeof(entry_addr);
+
+		/* Write zero size for entry addr */
+		sz = 0;
 		fwrite(&sz, sizeof(sz), 1, fd_out);
 		length += sizeof(sz);
-		fwrite(buf, sizeof(*buf), sz, fd_out);
-		length += sizeof(*buf) * sz;
+
+		/* Cleanup */
+		elf_end(elf_in);
+		close(fd_in);
 	}
-
-	/* Write entry addr */
-	entry_addr = ehdr.e_entry;
-	if (verbose)
-		printf("ENTR_ADDR=0x%8.8x\n", entry_addr);
-	fwrite(&entry_addr, sizeof(entry_addr), 1, fd_out);
-	length += sizeof(entry_addr);
-
-	/* Write zero size for entry addr */
-	sz = 0;
-	fwrite(&sz, sizeof(sz), 1, fd_out);
-	length += sizeof(sz);
 
 	/* Write magic at end of file */
 	fwrite(&magic, sizeof(magic), 1, fd_out);
@@ -167,11 +260,8 @@ int main(int argc, char **argv)
 	fseek(fd_out, sizeof(magic), SEEK_SET);
 	fwrite(&length, sizeof(length), 1, fd_out);
 
-	/* Cleanup */
-	elf_end(elf_in);
-	close(fd_in);
 	fclose(fd_out);
 
-	printf("Binary file %s generation complete.\n", argv[2]);
+	printf("Binary file %s generation complete.\n", outfile);
 	exit(EXIT_SUCCESS);
 }
