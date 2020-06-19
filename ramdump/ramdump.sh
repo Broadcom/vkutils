506504be144285fe64fb0bdf7b88c35b97313ec4
Mircea Carausu <mircea.carausu@broadcom.com>
2020-06-19 13:21:26 -0700

    SOC-11243: ramdump: add full parameter, limit size to 64MB default
    Allow to operate on /dev/bcm-vk.0  - default / no parameters
    Introduce a second parameter to control the ddr ramdump size
    By default capturing just the first 64MB of RAM.
    Use full parameter to indicate the full 2GB capture is needed
    Append the device_id (BCM_X) as file name suffix
    Update the usage instructions.
    
    Change-Id: I7153fea8f7d90de54ca68b435d916c75a2551592
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/124692
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Tested-by: Mircea Carausu <mircea.carausu@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Shamez Kurji <shamez.kurji@broadcom.com>

--- ramdump/ramdump.sh
+++ ramdump/ramdump.sh
@@ -2,47 +2,62 @@
 
 # The script will dump contents of RAM to a file
 # Which can be used for crash dump analysis
-# Timestamp is appended to the end of the filename
+# Timestamp and device_id are appended to the end of the filename
 
 # Command to reset the card from ramdump mode
-# vkcli /dev/bcm-vk.<dev_num> wb 0 0x41c 0xa0000000
+# vkcli <dev_num> reset
 
-# Code: 0xa0000000 => Code to reset the card from ramdump mode
 # Code: 0xb0000000 => Remap the PCIe BAR2 to the next 64MB of DDR
 # Code: 0xc0000000 => Remap the PCIe BAR2 to the default map base (0x60000000)
 
-if [ $# -ne 1 ]
+if [[ $1 == "--help" || $# -gt 2 ]]
 then
-        echo "Usage: ./ramdump.sh <dev_num>"
+        echo "Usage: ./ramdump.sh [dev_num] [full]"
+        echo -e ' \n\t '"dev_num: 0..11 - corresponds to /dev/bcm-vk.X; w/o dev_num = 0"
+        echo -e ' \t '"full: to capture full 2GB RAM image; w/o limit to first 64MB"
+        echo -e ' \n '
         exit 0
 fi
 
+VKCLI=vkcli
+
+dev_id=0
+if [[ $# -ge 1 ]]
+then
+        dev_id=$1
+fi
 echo "Dumping DTCM.."
-vkcli /dev/bcm-vk.$1 rf 1 0x0 0x40000 dtcm_dump_$(date +%Y%m%d-%H%M%S).bin
+$VKCLI $dev_id rf 1 0x0 0x40000 dtcm_dump_$(date +%Y%m%d-%H%M%S)_BCM_$dev_id.bin
 echo "Dumping ITCM.."
-vkcli /dev/bcm-vk.$1 rf 1 0x100000 0x40000 itcm_dump_$(date +%Y%m%d-%H%M%S).bin
+$VKCLI $dev_id rf 1 0x100000 0x40000 itcm_dump_$(date +%Y%m%d-%H%M%S)_BCM_$dev_id.bin
 echo "Dumping SCR SRAM.."
-vkcli /dev/bcm-vk.$1 rf 1 0x200000 0x40000 scr_sram_dump_$(date +%Y%m%d-%H%M%S).bin
+$VKCLI $dev_id rf 1 0x200000 0x40000 scr_sram_dump_$(date +%Y%m%d-%H%M%S)_BCM_$dev_id.bin
 
 echo "Dumping DDR.."
 # set the pcie window to default map base 0x60000000
-vkcli /dev/bcm-vk.$1 wb 0 0x41c 0xc0000000
+$VKCLI $dev_id wb 0 0x41c 0xc0000000
 
 # Dumps 2GB of DDR by remapping the pcie bar2 window
 # 2GB/64MB = 32
-for (( i=1; i <= 32; i++ ))
+max_slots=1
+if [[ $2 == "full" ]]
+then
+        max_slots=32
+fi
+
+for (( i=1; i <= $max_slots; i++ ))
 do {
   echo "---------------------------------"
   rm -rf ddr_dump_t.bin
-  vkcli /dev/bcm-vk.$1 rf 2 0x0 0x4000000 ddr_dump_t.bin
+  $VKCLI $dev_id rf 2 0x0 0x4000000 ddr_dump_t.bin
   # Request card to remap the pcie BAR2 to the next 64MB
-  vkcli /dev/bcm-vk.$1 wb 0 0x41c 0xb0000000
+  $VKCLI $dev_id wb 0 0x41c 0xb0000000
   cat ddr_dump_t.bin >> ddr_dump.bin
   echo $i
   echo "---------------------------------"
 } done
-mv ddr_dump.bin ddr_dump_$(date +%Y%m%d-%H%M%S).bin
+mv ddr_dump.bin ddr_dump_$(date +%Y%m%d-%H%M%S)_BCM_$dev_id.bin
 rm -rf ddr_dump_t.bin
 sync
 echo "RAMDUMP DONE"
-echo "To reset the card execute: vkcli /dev/bcm-vk.<dev_num> reset"
+echo "To reset the card execute: ${VKCLI} <dev_num> reset"
