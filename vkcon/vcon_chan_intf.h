15d602935ba5583dcb9e275eae91ba089099e5d3
Suji Velupillai <suji.velupillai@broadcom.com>
2020-07-07 17:13:16 -0700

    SOC-11922: vkcon: enhance logdump app to also parse console log file
    Made the following changes:
     * move logger parsing into a separate function
     * add new function to parse console log file
     * change filename size to 256
    Usage:
      vlogdump -f <logger file name>
      vlogdump -c <console log file name>
    
    Change-Id: I969567ba193fa016db0eb7f52cff196b42f210b2
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/125907
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Suji Velupillai <suji.velupillai@broadcom.com>
    Tested-by: Suji Velupillai <suji.velupillai@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Shamez Kurji <shamez.kurji@broadcom.com>

--- vkcon/vcon_chan_intf.h
+++ vkcon/vcon_chan_intf.h
@@ -24,7 +24,21 @@ typedef struct _logger_buf {
 #define VCON_BUF_BAR2_OFF	 0x3800000
 #define VCON_MARKER		 0xbeefcafe
 
+/**
+ * console structure
+ * This has to match the kernel structure definition: struct bptty_chnl
+ */
+typedef struct _console_buf {
+	uint32_t reserved;
+	uint32_t size;		/**< total size of the log */
+	uint32_t wr;		/**< console log writer index */
+	uint32_t rd;		/**< console log reader index */
+	uint32_t *data;		/**< console log data start addr */
+} console_buf;
+
+#define CONSOLE_ADDR(a)	(offsetof(console_buf, a))
+
 /* common file name max */
-#define FNAME_LEN		64
+#define FNAME_LEN		256
 
 #endif
--- vkcon/vcon_logdump.c
+++ vkcon/vcon_logdump.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,43 +24,95 @@
 /* checks for limits */
 #define MAX_NENTRIES    (10 * 1024)
 #define MAX_ENTRY_LEN   (512)
+#define MAX_CONSOLE_LEN (1 * 1024 * 1024)
 
-int main(int argc, char **argv)
+static int parse_console_log(char *f_name)
 {
 	FILE *fp;
-	char f_name[FNAME_LEN] = { 0 };
+	int ret;
+	long offset;
+	long rd_idx;
+	long wr_idx;
 	char c;
-	int option_index;
+	console_buf clog;
+
+	fp = fopen(f_name, "r");
+	if (!fp) {
+		_PR_LINE("Fail to open: %s error: %s\n",
+			 f_name, strerror(errno));
+		return errno;
+	}
+
+	ret = fseek(fp, 0, SEEK_SET);
+	if (ret < 0) {
+		_PR_LINE("Console header fseek failed: %s\n", strerror(errno));
+		goto fail;
+	}
+
+	ret = fread(&clog, sizeof(console_buf), 1, fp);
+	if (ret <= 0) {
+		_PR_LINE("Console header fread failed: %s\n", strerror(errno));
+		goto fail;
+	}
+
+	_PR_LINE(banner);
+	_PR_LINE("File %s - console buffer located\n", f_name);
+	_PR_LINE("  ==> size %d wr_idx %d rd_idx %d\n",
+		 clog.size, clog.wr, clog.rd);
+	_PR_LINE(banner);
+
+	/* sanity check the header info */
+	if (clog.size != (MAX_CONSOLE_LEN - CONSOLE_ADDR(data))) {
+		_PR_LINE("Console header size is invalid!\n");
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	if (clog.rd > clog.size || clog.wr > clog.size) {
+		_PR_LINE("Console header wr idx or/and rd idx invalid!\n");
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	/* reset the file pointer to read from last write index */
+	wr_idx = clog.wr + CONSOLE_ADDR(data);
+	rd_idx = wr_idx + 1;
+	rd_idx =  (rd_idx >= MAX_CONSOLE_LEN ? CONSOLE_ADDR(data) : rd_idx);
+	ret = fseek(fp, rd_idx, SEEK_SET);
+	if (ret < 0) {
+		_PR_LINE("Console data fseek failed: %s\n", strerror(errno));
+		goto fail;
+	}
+
+	while (rd_idx != wr_idx) {
+		c = fgetc(fp);
+
+		/* eof reached but not end of data, so loop back to start */
+		if (rd_idx >= MAX_CONSOLE_LEN || feof(fp)) {
+			rd_idx = CONSOLE_ADDR(data);
+			ret = fseek(fp, rd_idx, SEEK_SET);
+			if (ret < 0) {
+				_PR_LINE("Console fseek at %ld failed: %s\n",
+					 offset, strerror(errno));
+				goto fail;
+			}
+		}
+		rd_idx++;
+		_PR_LINE("%c", c);
+	}
+	printf("\n\n");
+
+fail:
+	fclose(fp);
+	return ret;
+}
+
+static int parse_logger(char *f_name)
+{
+	FILE *fp;
 	long offset = VCON_BUF_BAR2_OFF;
 	logger_buf log;
 	int ret, idx, i;
-
-	static struct option long_options[] = {
-		{"file", required_argument, 0, 'f'},
-		{0, 0, 0, 0}
-	};
-
-	while ((c = getopt_long(argc, argv, "f:",
-				long_options, &option_index)) != -1) {
-		switch (c) {
-		case 'f':
-			if (strlen(optarg) >= sizeof(f_name)) {
-				_PR_LINE("optarg too long for file name");
-				return -EINVAL;
-			}
-			strncpy(f_name, optarg, sizeof(f_name));
-			f_name[sizeof(f_name) - 1] = '\0';
-			break;
-		default:
-			_PR_LINE("%c Not supported", c);
-			return -EINVAL;
-		}
-	}
-
-	if (f_name[0] == '\0') {
-		_PR_LINE("Usage: %s -f <file name>\n", argv[0]);
-		return -EINVAL;
-	}
 
 	fp = fopen(f_name, "r");
 	if (!fp) {
@@ -79,6 +132,7 @@ int main(int argc, char **argv)
 		_PR_LINE("Fail reading logger structure\n");
 		goto fail;
 	}
+
 	_PR_LINE(banner);
 	_PR_LINE("File %s - spool buffer located\n", f_name);
 	_PR_LINE("  => entries 0x%x, idx %d len %d off 0x%x (marker 0x%x)\n",
@@ -96,8 +150,9 @@ int main(int argc, char **argv)
 	    !log.spool_len ||
 	    (log.spool_nentries > MAX_NENTRIES) ||
 	    (log.spool_len > MAX_ENTRY_LEN)) {
-		_PR_LINE("Dump file invalid....exit!\n");
-		return -EINVAL;
+		_PR_LINE("Fail: invalid logger header!\n");
+		ret = -EINVAL;
+		goto fail;
 	}
 
 	idx = (log.spool_idx + 1) & (log.spool_nentries - 1);
@@ -131,4 +186,73 @@ int main(int argc, char **argv)
 fail:
 	fclose(fp);
 	return ret;
+}
+
+static void usage(char *name)
+{
+	_PR_LINE("Usage:\n");
+	_PR_LINE("\t%s -f <logger file name>\n", name);
+	_PR_LINE("\t%s -c <console log file name>\n", name);
+}
+
+int main(int argc, char **argv)
+{
+	char f_name[FNAME_LEN] = { 0 };
+	char c;
+	int option_index;
+	int infile_f = 0;
+	int infile_c = 0;
+
+	static struct option long_options[] = {
+		{"file", required_argument, 0, 'f'},
+		{"console", required_argument, 0, 'c'},
+		{0, 0, 0, 0}
+	};
+
+	while ((c = getopt_long(argc, argv, "f:c:",
+				long_options, &option_index)) != -1) {
+		switch (c) {
+		case 'f':
+			if (strlen(optarg) >= sizeof(f_name)) {
+				_PR_LINE("optarg too long for file name\n");
+				return -EINVAL;
+			}
+			strncpy(f_name, optarg, sizeof(f_name));
+			f_name[sizeof(f_name) - 1] = '\0';
+			infile_f = 1;
+			break;
+		case 'c':
+			if (strlen(optarg) >= sizeof(f_name)) {
+				_PR_LINE("optarg too long for file name\n");
+				return -EINVAL;
+			}
+			strncpy(f_name, optarg, sizeof(f_name));
+			f_name[sizeof(f_name) - 1] = '\0';
+			infile_c = 1;
+			break;
+		default:
+			_PR_LINE("%c Not supported\n", c);
+			usage(argv[0]);
+			return -EINVAL;
+		}
+	}
+
+	if (infile_f && infile_c) {
+		_PR_LINE("Error: only one input file can be specified!\n");
+		usage(argv[0]);
+		return -EINVAL;
+	}
+
+	if (f_name[0] == '\0') {
+		usage(argv[0]);
+		return -EINVAL;
+	}
+
+	if (infile_c)
+		parse_console_log(f_name);
+
+	if (infile_f)
+		parse_logger(f_name);
+
+	return 0;
 }
