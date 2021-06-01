e9da6a47505f381fce73c08d462c895e21fa92a1
Jeffery Li <jeffery.li@broadcom.com>
2021-06-01 10:50:49 -0700

    Update to rel/vxc-1.3_1.3.1_GA4-RC12

--- vkshare/vkutil_msg.h
+++ vkshare/vkutil_msg.h
@@ -11,6 +11,7 @@
 
 pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 #endif
+#include <stdarg.h>
 
 /**
  * interface structure, and this has to match the VK side definition.
@@ -48,21 +49,42 @@ typedef struct _console_buf {
 #define FNAME_LEN		256
 #define MAX_ERR_MSG		512
 
-#define PERROR(...) do { \
-			snprintf(e_msg, \
-				 MAX_ERR_MSG, \
-				 __VA_ARGS__);\
-			fprintf(stderr, \
-				" @L:%d %s\n", \
-				__LINE__, \
-				e_msg);\
-			fflush(stderr);\
-			} while (0)
+static inline void fprintf_io(FILE *stream, const char *fmt, ...)
+{
+	int ret;
+	va_list vl;
 
-#define FPR_FN(...)             do { \
-					fprintf(stdout, __VA_ARGS__); \
-					fflush(stdout); \
-				} while (0)
+	va_start(vl, fmt);
+	ret = vfprintf(stream, fmt, vl);
+	va_end(vl);
+	if (ret < 0)
+		exit(EIO);
+}
+
+static inline void flush_io(FILE *stream)
+{
+	int ret;
+
+	ret = fflush(stream);
+	if (ret)
+		fprintf_io(stream, "Flush fails %d, errnor(%d)\n", ret, errno);
+}
+
+#define PERROR(...) do { \
+	int l_rc; \
+	char e_msg[MAX_ERR_MSG]; \
+	e_msg[0] = '\0'; \
+	l_rc = snprintf(e_msg, MAX_ERR_MSG, __VA_ARGS__); \
+	if ((l_rc >= sizeof(e_msg)) || (l_rc < 0)) \
+		fprintf_io(stderr, "Msg print returns error %d\n", l_rc); \
+	fprintf_io(stderr, " @L:%d %s\n", __LINE__, e_msg); \
+	flush_io(stderr); \
+} while (0)
+
+#define FPR_FN(...) do { \
+	fprintf_io(stdout, __VA_ARGS__); \
+	flush_io(stdout); \
+} while (0)
 
 #define _PR_F printf
 
@@ -72,11 +94,11 @@ typedef struct _console_buf {
 	printf(__VA_ARGS__);                \
 }
 #else
-#define _PR_LINE(...)                      \
-{                                          \
-	pthread_mutex_lock(&log_mutex);    \
+#define _PR_LINE(...)                       \
+{                                           \
+	pthread_mutex_lock(&log_mutex);     \
 	printf(__VA_ARGS__);                \
-	pthread_mutex_unlock(&log_mutex);  \
+	pthread_mutex_unlock(&log_mutex);   \
 }
 #endif
 #endif
